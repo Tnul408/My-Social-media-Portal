c083121ca320ffa62a75b048542783bf8cb83938
Jojos <sachaswoboda2@gmail.com>
2026-05-20 16:40:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ const myContents = [
         type: "ut",
         title: "Old school Gameplay",
         subtitle: "Channel: Old School Gameplay (Jojos)",
-        link: "https://www.youtube.com/watch?v=-V_8VFaXswE&t=144s",
-        image: "https://img.youtube.com/vi/-V_8VFaXswE&t=144s/maxresdefault.jpg"
+        link: "https://www.youtube.com/watch?v=https://www.youtube.com/watch?v=-V_8VFaXswE",
+        image: "https://img.youtube.com/vi/-V_8VFaXswE&t/maxresdefault.jpg"
     }
 ];
 
